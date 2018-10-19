7cc977222d9d7edebd3bcade473aae2844f58e0a
marcantoinethomas <36549789+marcantoinethomas@users.noreply.github.com>
2018-10-19 18:49:02 -0400

    MAJ MAT

---
+++ PiecesDeRechange/Models/Employe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PiecesDeRechange.Models
+{
+    public class Employe
+    {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string  Rue { get; set; }
+        public string NoApp { get; set; }
+        public string Ville { get; set; }
+        public string Province { get; set; }
+        public string CodePostal { get; set; }
+        public TypeEmploye Type { get; set; }
+        public string Identifiant { get; set; }
+        public string MotDePasse { get; set; }
+        public string Courriel { get; set; }
+    }
+}
