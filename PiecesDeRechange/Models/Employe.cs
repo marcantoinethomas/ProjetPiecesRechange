e2aa4f4a7df8f73fb0fbb57456bc60b1b3e4934e
marcantoinethomas <36549789+marcantoinethomas@users.noreply.github.com>
2018-10-19 19:07:28 -0400

    Last Update MAT

--- PiecesDeRechange/Models/Employe.cs
+++ PiecesDeRechange/Models/Employe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace PiecesDeRechange.Models
     public class Employe
     {
         public int ID { get; set; }
+
         public string Nom { get; set; }
         public string Prenom { get; set; }
         public string  Rue { get; set; }
