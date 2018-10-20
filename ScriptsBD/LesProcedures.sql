d7f12ea76da18b11665180d040ecc4e19e290213
marcantoinethomas <36549789+marcantoinethomas@users.noreply.github.com>
2018-10-20 18:07:58 -0400

    UP

--- ScriptsBD/LesProcedures.sql
+++ ScriptsBD/LesProcedures.sql
@@ -1,22 +1,56 @@
+USE SpareParts
+GO
 IF OBJECT_ID (N'GetListeEmployes', N'P') IS NOT NULL 
 	DROP PROCEDURE GetListeEmployes
 GO
 CREATE PROCEDURE GetListeEmployes
 AS
 BEGIN
-SELECT employeID ,
-nomEmp,
-prenomEmp ,
-rue ,
-noApp ,
-ville ,
-province ,
-codePostal ,
-typeEmpID ,
-courriel ,
-motDePasse ,
-statut 
-FROM Employe
+	SELECT employeID ,
+		nomEmp,
+		prenomEmp ,
+		rue ,
+		noApp ,
+		ville ,
+		province ,
+		codePostal ,
+		Employe.typeEmpID,
+		descripType= TypeEmploye.description,
+		courriel ,
+		motDePasse,
+		Employe.statut,
+		descripStatut= StatutEmploye.description 
+	FROM Employe 
+	INNER JOIN StatutEmploye ON Employe.statut= StatutEmploye.statutID
+	INNER JOIN TypeEmploye ON Employe.typeEmpID= TypeEmploye.typeEmpID
 ORDER BY nomEmp, prenomEmp
 END
+GO
+
+IF OBJECT_ID (N'CreerEmploye', N'P') IS NOT NULL 
+	DROP PROCEDURE CreerEmploye
+GO
+CREATE PROCEDURE CreerEmploye
+@nomEmp VARCHAR(50),
+@prenomEmp VARCHAR(50),
+@rue VARCHAR(50),
+@noApp VARCHAR(16),
+@ville VARCHAR(50),
+@province VARCHAR(50),
+@codePostal VARCHAR(16),
+@typeEmpID int,
+@courriel VARCHAR(50),
+@motDePasse VARCHAR(50)
+AS
+BEGIN
+	IF NOT EXISTS (SELECT * FROM Employe WHERE courriel= @courriel)
+		INSERT INTO Employe(nomEmp, prenomEmp, rue, noApp, ville, province,
+		codePostal, typeEmpID, courriel, motDePasse)
+		VALUES (@nomEmp, @prenomEmp, @rue, @noApp, @ville, @province,
+		@codePostal, @typeEmpID, @courriel, @motDePasse)
+END
+GO
+EXEC CreerEmploye 'Admin', 'Admin', null, null,null, null, null, 1, 'admin@gmail.com', 'admin'
+GO
+UPDATE Employe SET statut = 'A' WHERE courriel= 'admin@gmail.com'
 GO
